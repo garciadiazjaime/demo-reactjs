deeadd7b6271f30c663b26d1027a425d8e89cf20
Jaime <garciadiazjaime@gmail.com>
2024-11-17 15:46:32 -0600

    feat: decode jwt token

--- app/linkedin-api-openid-post-article/page.tsx
+++ app/linkedin-api-openid-post-article/page.tsx
@@ -2,6 +2,9 @@
 
 import { Suspense, useState, useEffect } from 'react'
 import { useSearchParams } from 'next/navigation'
+import { jwtDecode } from "jwt-decode";
+
+import Loader from '@/app/react-contact-form/components/loader'
 
 
 const LINKEDIN_CLIENT_ID = process.env.NEXT_PUBLIC_LINKEDIN_CLIENT_ID;
@@ -10,7 +13,7 @@ function Search({ setCode }: { setCode: (code: string) => void }) {
     const searchParams = useSearchParams()
 
     const code = searchParams.get('code') as string
-    console.log({ code })
+
     if (code) {
         setCode(code)
     }
@@ -22,9 +25,11 @@ function Search({ setCode }: { setCode: (code: string) => void }) {
 export default function Page() {
     const [code, setCode] = useState('')
     const [userInfo, setUserInfo] = useState('')
+    const [loading, setLoading] = useState(false)
 
     const getAuthorizationClickHandler = async (event: { preventDefault: () => void }) => {
         event.preventDefault()
+        setLoading(true)
 
         const callback = encodeURIComponent(`${location.origin}${location.pathname}`)
         window.location.replace(
@@ -46,9 +51,9 @@ export default function Page() {
             });
 
             const data = await response.json()
-            console.log(data)
-            const accessToken = data.access_token
-            sessionStorage.setItem('linkedin_access_token', accessToken)
+
+            sessionStorage.setItem('linkedin_access_token', data.access_token)
+            sessionStorage.setItem('linkedin_jwt_token', data.id_token)
 
             const responseUser = await fetch(`/.netlify/functions/linkedin-userinfo`, {
                 method: "GET",
@@ -57,24 +62,39 @@ export default function Page() {
                 },
             });
 
-            console.log({ responseUser })
-
             const user = await responseUser.json()
-            console.log(user)
+
             setUserInfo(user)
+            setLoading(false)
         }
 
         getUserInfo()
     }, [code])
 
+    useEffect(() => {
+        const jwt = sessionStorage.getItem('linkedin_jwt_token')
+
+        if (!jwt) {
+            return
+        }
+
+        const data = jwtDecode(jwt)
+
+        if (data) {
+            setUserInfo(data as unknown as string)
+        }
+    }, [])
+
 
     return <div style={{ maxWidth: 400, margin: "0 auto", padding: 12 }}>
-        <a href="" onClick={getAuthorizationClickHandler} style={{ padding: 20, border: '1px solid black', display: "block", textAlign: "center" }}>Authorize</a>
+        <h1 style={{ marginBottom: 20 }}>LinkedIn Get User Info</h1>
+        {!userInfo && <a href="" onClick={getAuthorizationClickHandler} style={{ padding: 20, border: '1px solid black', display: "block", textAlign: "center" }}>Authorize</a>}
         <Suspense>
             <Search setCode={setCode} />
         </Suspense>
         <div>
             <pre>{userInfo && JSON.stringify(userInfo, null, 2)}</pre>
         </div>
+        {loading && <Loader />}
     </div>
 }
