24c671f9ecfd18fd515518feaa7f2b91a74cf9ef
Jaime <garciadiazjaime@gmail.com>
2024-11-17 15:30:33 -0600

    feat: adjust flow

--- app/linkedin-api-openid-post-article/page.tsx
+++ app/linkedin-api-openid-post-article/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Suspense, useState } from 'react'
+import { Suspense, useState, useEffect } from 'react'
 import { useSearchParams } from 'next/navigation'
 
 
@@ -8,11 +8,12 @@ const LINKEDIN_CLIENT_ID = process.env.NEXT_PUBLIC_LINKEDIN_CLIENT_ID;
 
 function Search({ setCode }: { setCode: (code: string) => void }) {
     const searchParams = useSearchParams()
-    console.log({ searchParams, LINKEDIN_CLIENT_ID })
 
     const code = searchParams.get('code') as string
     console.log({ code })
-    setCode(code)
+    if (code) {
+        setCode(code)
+    }
 
     return <></>
 }
@@ -20,7 +21,7 @@ function Search({ setCode }: { setCode: (code: string) => void }) {
 
 export default function Page() {
     const [code, setCode] = useState('')
-    const [accessToken, setAccessToken] = useState('')
+    const [userInfo, setUserInfo] = useState('')
 
     const getAuthorizationClickHandler = async (event: { preventDefault: () => void }) => {
         event.preventDefault()
@@ -31,49 +32,49 @@ export default function Page() {
         );
     }
 
-    const getAccessTokenClickHandler = async (event: { preventDefault: () => void }) => {
-        event.preventDefault()
+    useEffect(() => {
+        if (!code) {
+            return
+        }
 
-        const response = await fetch(`/.netlify/functions/linkedin-authorization`, {
-            method: "POST",
-            body: JSON.stringify({
-                code,
-            }),
-        });
+        const getUserInfo = async () => {
+            const response = await fetch(`/.netlify/functions/linkedin-authorization`, {
+                method: "POST",
+                body: JSON.stringify({
+                    code,
+                }),
+            });
 
-        console.log({ response })
+            const data = await response.json()
+            console.log(data)
+            const accessToken = data.access_token
+            sessionStorage.setItem('linkedin_access_token', accessToken)
 
-        const data = await response.json()
-        console.log({ data })
+            const responseUser = await fetch(`/.netlify/functions/linkedin-userinfo`, {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${sessionStorage.getItem('linkedin_access_token')}`,
+                },
+            });
 
-        setAccessToken(data.accessToken)
-    }
+            console.log({ responseUser })
 
-    const getUserInfoClickHandler = async (event: { preventDefault: () => void }) => {
-        event.preventDefault()
+            const user = await responseUser.json()
+            console.log(user)
+            setUserInfo(user)
+        }
 
-        const response = await fetch(`/.netlify/functions/linkedin-userinfo`, {
-            method: "POST",
-            body: JSON.stringify({
-                accessToken,
-            }),
-        });
-
-        console.log({ response })
-
-        const data = await response.text()
-        console.log({ data })
-    }
+        getUserInfo()
+    }, [code])
 
 
     return <div style={{ maxWidth: 400, margin: "0 auto", padding: 12 }}>
         <a href="" onClick={getAuthorizationClickHandler} style={{ padding: 20, border: '1px solid black', display: "block", textAlign: "center" }}>Authorize</a>
-        <br />
-        <a href="" onClick={getAccessTokenClickHandler} style={{ padding: 20, border: '1px solid black', display: "block", textAlign: "center" }}>Get Token</a>
-        <br />
-        <a href="" onClick={getUserInfoClickHandler} style={{ padding: 20, border: '1px solid black', display: "block", textAlign: "center" }}>Get User Info</a>
         <Suspense>
             <Search setCode={setCode} />
         </Suspense>
+        <div>
+            <pre>{userInfo && JSON.stringify(userInfo, null, 2)}</pre>
+        </div>
     </div>
 }
